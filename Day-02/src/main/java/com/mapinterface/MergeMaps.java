88234a8a42f5473c5f7a6c47dd4701214a0a405e
Prachiti <prachitiagarkar1@gmail.com>
2025-02-13 10:28:39 +0530

    [Prachiti Agarkar] add: Merge two maps

---
+++ Day-02/src/main/java/com/mapinterface/MergeMaps.java
@@ -0,0 +1,30 @@
+package com.mapinterface;
+
+import java.util.*;
+
+// Class to merge two maps and sum values for common keys
+class MapMerger {
+    public static Map<String, Integer> mergeMaps(Map<String, Integer> map1, Map<String, Integer> map2) {
+        Map<String, Integer> mergedMap = new HashMap<>(map1);
+
+        for (Map.Entry<String, Integer> entry : map2.entrySet()) {
+            mergedMap.merge(entry.getKey(), entry.getValue(), Integer::sum);
+        }
+
+        return mergedMap;
+    }
+
+    public static void main(String[] args) {
+        Map<String, Integer> map1 = new HashMap<>();
+        map1.put("A", 1);
+        map1.put("B", 2);
+
+        Map<String, Integer> map2 = new HashMap<>();
+        map2.put("B", 3);
+        map2.put("C", 4);
+
+        Map<String, Integer> mergedMap = mergeMaps(map1, map2);
+        System.out.println("Merged Map: " + mergedMap);
+    }
+}
+
