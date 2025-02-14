eb9d8596a2078bf8a683d5a9aab9d9423f896e85
Prachiti <prachitiagarkar1@gmail.com>
2025-02-14 13:53:05 +0530

    [Prachiti Agarkar]add: Exception propogation

---
+++ Day-04/src/main/java/ExceptionPropogation.java
@@ -0,0 +1,17 @@
+public class ExceptionPropogation {
+    public static void method1() throws ArithmeticException{
+        int ans=10/0;
+    }
+    public static void method2(){
+        method1();
+    }
+
+    public static void main(String[] args) {
+        try{
+            method2();
+        }
+        catch (Exception e){
+            System.out.println("Handled exception in main");
+        }
+    }
+}
