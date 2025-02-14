6834ad7f787dcd2c01f77643fcb85ecfe7bc56d7
Prachiti <prachitiagarkar1@gmail.com>
2025-02-14 13:35:16 +0530

    [Prachiti Agarkar] add: Piped Stream

---
+++ Day-03/src/main/java/com/pipedstream/PipedStream.java
@@ -0,0 +1,62 @@
+package com.pipedstream;
+
+import java.io.*;
+
+class WriterThread extends Thread {
+    private PipedOutputStream pos;
+
+    public WriterThread(PipedOutputStream pos) {
+        this.pos = pos;
+    }
+
+    @Override
+    public void run() {
+        try {
+            String message = "Hello from WriterThread!";
+            pos.write(message.getBytes());
+            pos.close();
+        } catch (IOException e) {
+            System.err.println("WriterThread Error: " + e.getMessage());
+        }
+    }
+}
+
+class ReaderThread extends Thread {
+    private PipedInputStream pis;
+
+    public ReaderThread(PipedInputStream pis) {
+        this.pis = pis;
+    }
+
+    @Override
+    public void run() {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(pis));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println("ReaderThread received: " + line);
+            }
+            pis.close();
+        } catch (IOException e) {
+            System.err.println("ReaderThread Error: " + e.getMessage());
+        }
+    }
+}
+
+public class PipedStream {
+    public static void main(String[] args) {
+        try {
+            PipedOutputStream pos = new PipedOutputStream();
+            PipedInputStream pis = new PipedInputStream(pos);
+
+            WriterThread writer = new WriterThread(pos);
+            ReaderThread reader = new ReaderThread(pis);
+
+            writer.start();
+            reader.start();
+        } catch (IOException e) {
+            System.err.println("Error setting up piped streams: " + e.getMessage());
+        }
+    }
+}
+
