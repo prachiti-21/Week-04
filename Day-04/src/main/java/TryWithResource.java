9f39d78ea5e53fe60f29f2133367f42173e401b4
Prachiti <prachitiagarkar1@gmail.com>
2025-02-14 13:56:04 +0530

    [Prachiti Agarkar]add: Try with resource

---
+++ Day-04/src/main/java/TryWithResource.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class TryWithResource {
+    public static void main(String[] args) {
+
+
+        try (BufferedReader br = new BufferedReader(new FileReader("info.txt"))) {
+            String line=br.readLine();
+            if(line != null) {
+                System.out.println(line);
+
+            }
+
+        } catch (IOException e){
+            //System.out.println(e.getMessage());
+            System.out.println("Error reading file");
+            //.printStackTrace();
+        }
+    }
+}
