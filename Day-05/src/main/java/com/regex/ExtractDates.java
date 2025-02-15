6e87fd68901b7a287475223a014551a4450fced8
Prachiti <prachitiagarkar1@gmail.com>
2025-02-15 12:17:05 +0530

    [Prachiti Agarkar] add: Extract dates from a text

---
+++ Day-05/src/main/java/com/regex/ExtractDates.java
@@ -0,0 +1,21 @@
+package com.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractDates {
+    public static void extractDates(String text){
+        String regex="\\b(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/([0-9]{4})\\b";
+        Pattern pattern =Pattern.compile(regex);
+        Matcher matcher=pattern.matcher(text);
+        while(matcher.find()){
+            System.out.print(matcher.group()+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        String text="The events are scheduled for 12/05/2023, 15/08/2024, and 29/02/2020.";
+        extractDates(text);
+
+    }
+}
